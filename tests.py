8e1dadc7db8c8438922e93f783dab4c02e617e02
Pavel Hübner <arxarian@gmail.com>
2025-05-10 22:14:14 +0200

    drive back and forth, reverse on yellow and red
    Signed-off-by: Pavel Hübner <arxarian@gmail.com>

--- tests.py
+++ tests.py
@@ -6,34 +6,37 @@ from pybricks.tools import wait, StopWatch
 import colors
 
 sensor = ColorDistanceSensor(Port.B)
-#train_motor = DCMotor(Port.A)
+train_motor = DCMotor(Port.A)
+direction = True
 
-#def stop():
-#    train_motor.dc(0)
+def highSpeed():
+    train_motor.dc(30 if direction else -30)
 
-#def highSpeed():
-#    train_motor.dc(40)
+def lowSpeed():
+    train_motor.dc(30 if direction else -30)
 
-#def lowSpeed():
-#    train_motor.dc(50)
+def stop():
+    train_motor.dc(0)
+    wait(500)
+    lowSpeed()
+    wait(500)
 
-
-#lowSpeed()
+lowSpeed()
 
 while True:
+    hsv = sensor.hsv()
+    color = colors.decodeHSV(hsv)
+    if color != Color.NONE:
+        print(color, hsv)
 
-    if sensor.distance() < 15:
-        color = colors.decodeHSV(sensor.hsv())
-        if color != Color.NONE:
-            print(color)
+
+    if color == Color.RED:
+        direction = False
+        stop()
+    elif color == Color.YELLOW:
+        direction = True
+        stop()
+
+    lowSpeed()
 
     wait(10)
-
-#    if color == Color.RED:
-#        stop()
-#        wait(3000)
-#    elif color == Color.GREEN:
-#        highSpeed()
-#    elif color == Color.YELLOW:
-#        lowSpeed()
-
