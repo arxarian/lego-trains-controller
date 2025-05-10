9ce1ecfe8ba80457be248956447a55b6bf0667ed
Pavel Hübner <arxarian@gmail.com>
2025-05-10 21:48:59 +0200

    add more colors and use saturation for detection
    Signed-off-by: Pavel Hübner <arxarian@gmail.com>

--- colors.py
+++ colors.py
@@ -1,22 +1,23 @@
 from pybricks.parameters import Color
 
 def decodeHSV(hsv):
-    print(hsv)
+    #print(hsv)
     hue = hsv.h
+    saturation = hsv.s
     value = hsv.v
-    if (hue > 340 or hue < 20) and value < 90 and value > 70:
+    if value < 20 and saturation < 40:
+        return Color.BLACK
+    elif (hue > 180 or hue < 240) and value > 50 and value < 70 and saturation < 30:
+        return Color.GRAY
+    elif (hue > 340 or hue < 20) and value > 70 and value < 90:
         return Color.RED
-    elif (hue > 30 and hue < 70) and value > 80:
+    elif (hue > 30 and hue < 70) and value > 80 and saturation > 60:
         return Color.YELLOW
-    elif (hue > 200 and hue < 250) and value > 35:
+    elif (hue > 200 and hue < 250) and value > 35 and saturation > 80:
         return Color.BLUE
     elif (hue > 100 and hue < 150) and value > 35:
         return Color.GREEN
     elif (hue > 340 or hue < 30) and value > 90:
         return Color.WHITE
-    elif value < 20:
-        return Color.BLACK
-    #elif (hue > 340 or hue < 30) and value > 20 and value < 80:
-    #    return Color.GRAY
 
     return Color.NONE
