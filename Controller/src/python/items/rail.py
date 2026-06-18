bdc517ee1de3c8fdafc2164579f6573896379a8a
Pavel Hübner <arxarian@gmail.com>
2026-06-18 23:15:44 +0200

    122: fix Markers constrution
    Signed-off-by: Pavel Hübner <arxarian@gmail.com>

--- Controller/src/python/items/rail.py
+++ Controller/src/python/items/rail.py
@@ -85,8 +85,8 @@ class Rail(QObject):
         self._rotator = rotator if rotator is not None else Rotator(parent=self)    # Rotator/QObject
 
         # QAbstractListModels
+        self._markers = markers if markers is not None else Markers(parent=self)
         self._connectors = connectors if connectors is not None else Connectors(parent=self)
-        self._markers = markers if markers is not None else Markers(connectors=self._connectors, parent=self)
         self._path_indicators = PathIndicators(parent=self)
         self._reservation_indicators = PathIndicators(parent=self)
         self._reserved = False  # reserved by train
